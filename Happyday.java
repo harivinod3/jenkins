3fce4c2205c19837bda3b61a1d9ab1dca59a4ed0
harivinod3 <harivinod3@users.noreply.github.com>
2018-12-08 14:30:39 +0530

    Update and rename Helloworld to Happyday.java

--- Happyday.java
+++ Happyday.java
@@ -1,4 +1,4 @@
-public class Helloworld {
+public class Happyday {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
